e4250ffa29c4cffbb0e4f2f2ecde3e1f62a25183
Ayomide <ilesanmibry@gmail.com>
2024-06-26 05:21:33 +0100

    fix: fied bug in code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,18 +3,18 @@
 
 
 def pascal_triangle(n):
-  """eturns a list of lists of integers
-  representing the Pascal’s triangle of n"""
-  if n <= 0:
-    return []
+    """eturns a list of lists of integers
+    representing the Pascal’s triangle of n"""
+    if n <= 0:
+        return []
 
-  triangle = [[1]]
+    triangle = [[1]]
 
-  for i in range(1, n):
-    row = [1]
-    for j in range(1, i):
-      row.append(triangle[i-1][j-1] + triangle[i-1][j])
-    row.append(1)
-    triangle.append(row)
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+        row.append(1)
+        triangle.append(row)
 
-  return triangle
+    return triangle
